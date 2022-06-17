fa5250d91c153cc2ae00e88ae4e1d1805f1c5739
Adam Parker <adam@kirschbaumdevelopment.com>
2022-06-17 17:31:17 -0400

    resource updates to support Nova v4

--- resources/js/fields.js
+++ resources/js/fields.js
@@ -1,6 +1,11 @@
-Nova.booting(Vue => {
-    Vue.component('form-send-mail', require('./components/send/FormField'));
-    Vue.component('index-events', require('./components/events/IndexField'));
-    Vue.component('detail-events', require('./components/events/DetailField'));
-    Vue.component('form-events', require('./components/events/FormField'));
-});
+import FormSendMailField from './components/send/FormField'
+import IndexEventsField from './components/events/IndexField'
+import DetailEventsField from './components/events/DetailField'
+import FormEventsField from './components/events/FormField'
+
+Nova.booting((app, store) => {
+    app.component('form-send-mail', FormSendMailField);
+    app.component('index-events', IndexEventsField);
+    app.component('detail-events', DetailEventsField);
+    app.component('form-events', FormEventsField);
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 let mix = require('laravel-mix')
 
-mix.setPublicPath('dist')
+require('./nova.mix')
+
+mix
+    .setPublicPath('dist')
     .js('resources/js/fields.js', 'js')
+    .vue({ version: 3 })
     .sass('resources/sass/fields.scss', 'css')
+    .nova('/kirschbaum-development/nova-mail')
